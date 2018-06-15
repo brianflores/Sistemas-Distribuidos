7928b1d14c6466eaca74df18384f54ec114df365
Brian Flores <brianf_lp@hotmail.com>
2018-06-15 14:46:26 -0300

    Update ejercicio2.c

--- entrega2/ejercicio2.c
+++ entrega2/ejercicio2.c
@@ -25,7 +25,7 @@ int main(int argc, char** argv) {
     }
     int N = atol(argv[1]);
     int ID, cantProcesos;
-    omp_set_num_threads(1);
+    omp_set_num_threads(4);
     MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ID);
 	MPI_Comm_size(MPI_COMM_WORLD, &cantProcesos);
